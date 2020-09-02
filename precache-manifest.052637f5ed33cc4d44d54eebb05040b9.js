3742ce4e600dd0e118d717f6e1dfe869a509a821
kjay <kcdhopkins@gmail.com>
2020-09-02 12:45:52 -0500

    update0001

---
+++ precache-manifest.052637f5ed33cc4d44d54eebb05040b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa52a9b065117a633ef0112cfe6f24d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "929aea4d1198b7ebc857",
+    "url": "/static/css/2.5b44f0a0.chunk.css"
+  },
+  {
+    "revision": "d04ce7919af65142bc66",
+    "url": "/static/css/main.80102a00.chunk.css"
+  },
+  {
+    "revision": "929aea4d1198b7ebc857",
+    "url": "/static/js/2.a71a6c98.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.a71a6c98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d04ce7919af65142bc66",
+    "url": "/static/js/main.81e34f76.chunk.js"
+  },
+  {
+    "revision": "e8f6e4bbaba917b7690d",
+    "url": "/static/js/runtime-main.34002bc1.js"
+  },
+  {
+    "revision": "5f48353a41e38955e316d425446b729c",
+    "url": "/static/media/jumbotron.5f48353a.jpg"
+  },
+  {
+    "revision": "4b5bcfc148b8108a26ba277f32d1f8c9",
+    "url": "/static/media/me.4b5bcfc1.jpg"
+  }
+]);
